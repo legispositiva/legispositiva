3df3e88cac9dd0ac793687412d385297c47029c7
Shane Kilkelly <shane@kilkelly.me>
2014-10-25 11:08:05 +0100

    exploring how foreign-key relations work in mongoose

--- server/api/document/document.model.js
+++ server/api/document/document.model.js
@@ -2,10 +2,20 @@
 
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+var ObjectId = mongoose.Schema.Types.ObjectId;
 
 var DocumentSchema = new Schema({
     documentId: String,
     timestamp: Date,
-    articles: [ObjectId],
-    ammends: ObjectId
+    articles: [{ type: ObjectId, ref: 'Article' }],
+    _ammends: { type: ObjectId, ref: 'Document' },
+    _creator: { type: ObjectId, ref: 'User' }
 })
+
+/**
+ * Virtuals
+ */
+// DocumentSchema
+//   .virtual('')
+
+module.exports = mongoos.model('Document', DocumentSchema)
