8c34a1ad88cf0bc7058716148ce7df14bb4d5e7f
Shane Kilkelly <shane@kilkelly.me>
2014-10-26 17:24:35 +0000

    make the formatting a bit less mental.

--- server/api/document/document.model.js
+++ server/api/document/document.model.js
@@ -6,30 +6,41 @@ var ObjectId = mongoose.Schema.Types.ObjectId;
 
 var DocumentSchema = new Schema({
 
-    documentId: { type: String,
-                  required: true,
-                  index: true },
+    documentId: {
+        type: String,
+        required: true,
+        index: true
+    },
 
-    timestamp:  { type: Date,
-                  required: true,
-                  index: true },
+    timestamp: {
+        type: Date,
+        required: true,
+        index: true
+    },
 
-    title:      { type: String,
-                  required: true},
+    title: {
+        type: String,
+        required: true
+    },
 
-    articles:   [
+    articles: [
         { type: ObjectId, ref: 'Article' }
     ],
 
-    _ammends:   { type: ObjectId,
-                  ref: 'Document',
-                  required: true,
-                  index: true },
+    _ammends: {
+        type: ObjectId,
+        ref: 'Document',
+        required: true,
+        index: true
+    },
 
-    _creator:   { type: ObjectId,
-                  ref: 'User',
-                  required: true,
-                  index: true }
+    _creator: {
+        type: ObjectId,
+        ref: 'User',
+        required: true,
+        index: true
+    }
+
 })
 
 /**
