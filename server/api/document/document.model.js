0ec9178607837221c58e512d662faa525db8b6e8
Shane Kilkelly <shane@kilkelly.me>
2014-10-26 17:22:21 +0000

    Add indexes and other nice things

--- server/api/document/document.model.js
+++ server/api/document/document.model.js
@@ -5,14 +5,31 @@ var Schema = mongoose.Schema;
 var ObjectId = mongoose.Schema.Types.ObjectId;
 
 var DocumentSchema = new Schema({
-    documentId: String,
-    timestamp:  Date,
-    title:      String,
+
+    documentId: { type: String,
+                  required: true,
+                  index: true },
+
+    timestamp:  { type: Date,
+                  required: true,
+                  index: true },
+
+    title:      { type: String,
+                  required: true},
+
     articles:   [
         { type: ObjectId, ref: 'Article' }
     ],
-    _ammends:   { type: ObjectId, ref: 'Document' },
-    _creator:   { type: ObjectId, ref: 'User' }
+
+    _ammends:   { type: ObjectId,
+                  ref: 'Document',
+                  required: true,
+                  index: true },
+
+    _creator:   { type: ObjectId,
+                  ref: 'User',
+                  required: true,
+                  index: true }
 })
 
 /**
@@ -21,4 +38,4 @@ var DocumentSchema = new Schema({
 // DocumentSchema
 //   .virtual('')
 
-module.exports = mongoos.model('Document', DocumentSchema)
+module.exports = mongoose.model('Document', DocumentSchema)
